e5789b5039ce6dab1c4845b0cec359940892f981
arnautovd <arnautov.dmitry2015@yandex.ru>
2020-10-02 15:00:57 +0300

    New addition for function in js

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
 const func = (a, b) => {
-    return a + b;
+    let len = (a + b).toString().length;
+    if (len > 1) {
+        let arr = (a + b).toString().split('');
+        console.log(arr);
+    }
+    else {
+        console.log("We get problems");
+    }
 }
-console.log(func(1,2));
+
+console.log(func(10,120));
